612242f9eb61c9d5943882805959db67c9ee1fb3
Teresa-Macarulla <teresm@uio.no>
2025-08-31 13:02:13 +0200

    Project1

---
+++ P1/plot.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Read two-column data: x y
+x, y = np.loadtxt("data.txt", unpack=True)
+
+plt.plot(x, y)
+plt.xlabel("x")
+plt.ylabel("u(x)")
+plt.grid(True)
+plt.tight_layout()
+plt.savefig("plot.pdf")  # saves to PDF
+# plt.show()  # optional
